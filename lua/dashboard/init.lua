a41147e49293862fa5695d0f9a11d2293da09200
Daniel Bevan <20234823+Dbevan770@users.noreply.github.com>
2024-03-30 20:13:10 -0500

    Initial dashboard plugin design
    - Added the init.lua file.
      - Created setup and open_dashboard functions

---
+++ lua/dashboard/init.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+function M.setup()
+	-- Automatically open the dashboard when Neovim starts with an empty buffer
+	vim.api.nvim_create_autocmd("VimEnter", {
+		pattern = "*",
+		callback = function()
+			if vim.fn.argc() == 0 and vim.fn.line2byte("$") == -1 then
+				M.open_dashboard()
+			end
+		end,
+	})
+end
+
+function M.open_dashboard()
+	-- Clear the buffer and set some basic content
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, { "Welcome to my Dashboard", "More features coming soon!" })
+	-- Set buffer options to make it look like a Dashboard
+	vim.api.nvim_buf_set_option(0, "buftype", "nofile")
+	vim.api.nvim_buf_set_option(0, "bufhidden", "hide")
+	vim.api.nvim_buf_set_option(0, "swapfile", false)
+end
+
+return M
