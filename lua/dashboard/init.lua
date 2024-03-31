17ec22a1ba1c81621b5cb97168eec2f62039b5b7
Daniel Bevan <20234823+Dbevan770@users.noreply.github.com>
2024-03-30 20:50:45 -0500

    Create BufLeave AutoCommand
    - BufLeave autocommand used to reset global options to the ones the user
    has set in their config.

--- lua/dashboard/init.lua
+++ lua/dashboard/init.lua
@@ -13,6 +13,10 @@ function M.setup()
 end
 
 function M.open_dashboard()
+	-- Stored global options
+	local global_number = vim.api.nvim_get_option("number")
+	local global_relativenumber = vim.api.nvim_get_option("relativenumber")
+
 	-- Clear the buffer and set some basic content
 	vim.api.nvim_buf_set_lines(0, 0, -1, false, { "Welcome to my Dashboard", "More features coming soon!" })
 
@@ -25,6 +29,16 @@ function M.open_dashboard()
 	vim.api.nvim_buf_set_option(0, "bufhidden", "hide")
 	vim.api.nvim_buf_set_option(0, "readonly", true)
 	vim.api.nvim_buf_set_option(0, "swapfile", false)
+
+	-- Reset line number and relative line number settings to the user's settings when leaving
+	-- the buffer
+	vim.api.nvim_create_autocmd("BufLeave", {
+		buffer = 0,
+		callback = function()
+			vim.api.nvim_set_option("number", global_number)
+			vim.api.nvim_set_option("relativenumber", global_relativenumber)
+		end,
+	})
 end
 
 return M
